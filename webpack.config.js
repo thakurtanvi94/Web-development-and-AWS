e428470967eb86480383a61bfbeff097e81101d4
thakurtanvi94 <78815946+thakurtanvi94@users.noreply.github.com>
2021-05-24 14:35:45 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue?$/,
+                exclude: /(node_modules)/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }, {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000',
+                options: {
+                    esModule: false,
+                },
+            }
+            // {
+            //     test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+            //     loader: 'url-loader',
+            //     options: {
+            //         limit: 25000,
+            //     },
+            // }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
